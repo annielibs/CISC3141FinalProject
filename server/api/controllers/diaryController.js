7d298f5f4f7fdbe286a1cad835524f55ebecc621
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2021-12-03 14:55:39 -0500

    refactored diary controller format and updated gitignore

--- server/api/controllers/diaryController.js
+++ server/api/controllers/diaryController.js
@@ -10,100 +10,82 @@ const Diaries = require("../../db/models/diaries");
 */
 
 // READ all diaries from database
-const getAllDiaries = (req, res) => {
-  Diaries.findAll() // SELECT * FROM ...
-    .then((diaries) => {
-      res.status(200).send(diaries);
-      console.log("Successfully retrieved diaries from database");
-    })
-    .catch((err) => {
-      res.status(500).send("Unable to retrieve user diaries");
-      console.log("Unsuccessful operation retrieving diaries from database", err);
-    });
+const getAllDiaries = async (req, res) => {
+    try {
+        const diaries = await Diaries.findAll();
+
+        res.status(200).send(diaries);
+        console.log("Successfully retrieved diaries from database");
+    } catch (err) {
+        res.status(500).send("Unable to retrieve user diaries");
+        console.log("Unsuccessful operation retrieving diaries from database", err);
+    }
 };
 
 // READ single diary
-const getDiaryByName = (req, res) => {
-  const diaryName = req.params.diaryName;
+const getDiaryByName = async (req, res) => {
+    try {
+        const diaryName = req.params.diaryName;
+        const diary = await Diaries.findOne({ where: { diary_name: diaryName } });
 
-  Diaries.findOne({
-    where: { diary_name: diaryName },
-  })
-    .then((diary) => {
-      res.status(200).send(diary);
-      console.log(`Successfully retrieved diary with name "${diaryName}"`);
-    })
-    .catch((err) =>
-      res.status(500).send(`Unsuccessful operation retrieving diary with name "${diaryName}"`, err)
-    );
+        res.status(200).send(diary);
+        console.log(`Successfully retrieved diary with name "${diaryName}"`);
+    } catch (err) {
+        res.status(500).send(`Unable to find diary with name "${diaryName}"`, err);
+        console.log(`Unsuccessful operation retrieving diary with name "${diaryName}"`);
+    }
 };
 
 // CREATE single new diary
-const createSingleDiary = (req, res) => {
-  const diaryName = req.body.diary_name; // get diary name from client request
+const createSingleDiary = async (req, res) => {
+    try {
+        const diaryName = req.body.diary_name; // get diary name from client request
+        await Diaries.create({
+            diary_name: diaryName,
+            diary_creation_date: new Date().toISOString().slice(0, 10),  // YYYY-MM-DD
+        });
 
-  // create new Diary object save to database
-  Diaries.create({
-    diary_name: diaryName,
-    diary_creation_date: new Date().toISOString().slice(0, 10),
-  })
-    .then(() => {
-      res.status(200).send(`Created new diary: "${diaryName}"`);
-      console.log(`Successfully saved new diary "${diaryName}" to database`);
-    })
-    .catch((err) => {
-      res.status(500).send(`"Unable to create new diary: "${diaryName}"`);
-      console.log(`Unsuccessful operation to CREATE "${diaryName}" and save to database`, err);
-    });
+        res.status(200).send(`Created new diary: "${diaryName}"`);
+        console.log(`Successfully saved new diary "${diaryName}" to database`);
+    } catch (err) {
+        res.status(500).send(`"Unable to create new diary: "${diaryName}"`);
+        console.log(`Unsuccessful operation to CREATE "${diaryName}" and save to database`, err);
+    }
 };
 
 // UPDATE diary information
-const updateSingleDiary = (req, res) => {
-  const diaryName = req.params.diaryName;
-  const newDiaryName = req.body.newDiaryName;
+const updateSingleDiary = async (req, res) => {
+    try {
+        const diaryName = req.params.diaryName;
+        const newDiaryName = req.body.newDiaryName;
+        await Diaries.update({ diary_name: newDiaryName }, { where: { diary_name: diaryName } });
 
-  Diaries.update(
-    { diary_name: newDiaryName }, // updated value
-    {
-      where: {
-        diary_name: diaryName, // specify diaries to update
-      },
+        res.status(200).send(`Updated diary: "${diaryName}" to "${newDiaryName}"`);
+        console.log(`Successfully updated "${diaryName}" to "${newDiaryName}" in database`);
+    } catch (err) {
+        res.status(500).send(`Unable to updated diary: "${diaryName}"`);
+        console.log(`Unsucessful operation to UPDATE "${diaryName}" in database`, err);
     }
-  )
-    .then(() => {
-      res.status(200).send(`Updated diary: "${diaryName}" to "${newDiaryName}"`);
-      console.log(`Successfully updated "${diaryName}" to "${newDiaryName}" in database`);
-    })
-    .catch((err) => {
-      res.status(500).send(`Unable to updated diary: "${diaryName}"`);
-      console.log(`Unsucessful operation to UPDATE "${diaryName}" in database`, err);
-    });
 };
 
 // DELETE single diary
-const deleteSingleDiary = (req, res) => {
-  const diaryName = req.params.diaryName;
+const deleteSingleDiary = async (req, res) => {
+    try {
+        const diaryName = req.params.diaryName;
+        await Diaries.destroy({ where: { diary_name: diaryName } });
 
-  // delete based on diary name   -> should names be unique? or delete by id?
-  Diaries.destroy({
-    where: {
-      diary_name: diaryName,
-    },
-  })
-    .then(() => {
-      res.status(200).send(`Deleted diary: "${diaryName}"`);
-      console.log(`Successfully deleted "${diaryName}" from database`);
-    })
-    .catch((err) => {
-      res.status(500).send(`Unable to delete diary: "${diaryName}"`);
-      console.log(`Unsuccessful operation to DELETE "${diaryName}" from database`, err);
-    });
+        res.status(200).send(`Deleted diary: "${diaryName}"`);
+        console.log(`Successfully deleted "${diaryName}" from database`);
+    } catch (err) {
+        res.status(500).send(`Unable to delete diary: "${diaryName}"`);
+        console.log(`Unsuccessful operation to DELETE "${diaryName}" from database`, err);
+    }
 };
 
 module.exports = {
-  getAllDiaries,
-  getDiaryByName,
-  createSingleDiary,
-  deleteSingleDiary,
-  updateSingleDiary,
+    getAllDiaries,
+    getDiaryByName,
+    createSingleDiary,
+    deleteSingleDiary,
+    updateSingleDiary,
 };
