db837a98da99ebb8539088e95d8f329fd69f9a9b
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2021-12-05 16:45:03 -0500

    add procfile

--- server/db/db.js
+++ server/db/db.js
@@ -3,13 +3,26 @@ require("dotenv").config(); // requries dotenv
 // for process.env.DATABASE_PASS either out password here it make an .env file and place password there/
 
 const db = new Sequelize(
-    process.env.DATABASE_NAME,
-    process.env.DATABASE_USER,
-    process.env.DATABASE_PASS,
+    "postgres://rnbkflhwmjywyg:800fdaf427eb957861fb10bdcca65f205530e216d3a2dc48578e50f5153bfce8@ec2-3-214-136-47.compute-1.amazonaws.com:5432/d8jbh2eethe4io",
     {
-        host: "localhost",
         dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
     }
 );
+
+// const db = new Sequelize(
+//     process.env.DATABASE_NAME,
+//     process.env.DATABASE_USER,
+//     process.env.DATABASE_PASS,
+//     {
+//         host: "localhost",
+//         dialect: "postgres",
+//     }
+// );
 
 module.exports = db;
