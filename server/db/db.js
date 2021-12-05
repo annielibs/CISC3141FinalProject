73747b10e5a1752cb7fbfece2e5fba4c53179e82
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2021-12-05 16:59:04 -0500

    edit connect url

--- server/db/db.js
+++ server/db/db.js
@@ -2,18 +2,15 @@ const Sequelize = require("sequelize");
 require("dotenv").config(); // requries dotenv
 // for process.env.DATABASE_PASS either out password here it make an .env file and place password there/
 
-const db = new Sequelize(
-    "postgres://rnbkflhwmjywyg:800fdaf427eb957861fb10bdcca65f205530e216d3a2dc48578e50f5153bfce8@ec2-3-214-136-47.compute-1.amazonaws.com:5432/d8jbh2eethe4io",
-    {
-        dialect: "postgres",
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
         },
-    }
-);
+    },
+});
 
 // const db = new Sequelize(
 //     process.env.DATABASE_NAME,
