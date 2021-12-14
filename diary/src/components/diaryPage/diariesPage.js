5a6c94b69abbc2d54dea151158ac9e9973425d0c
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2021-12-14 12:17:51 -0500

    diary delete button

--- diary/src/components/diaryPage/diariesPage.js
+++ diary/src/components/diaryPage/diariesPage.js
@@ -8,25 +8,52 @@ import "../../styles/DiaryStyles/DiaryPage.css";
 // Button component opens Modal Form to create new Diary
 const CreateDiaryButton = ({ createBtnHandler }) => {
   return (
-    <div className="modal-popup-btn-container">
+    <>
       <button className="diary-modal-popup-btn" onClick={createBtnHandler} type="button">
         Create a Diary
       </button>
-    </div>
+    </>
+  );
+};
+
+const EditDiaryButton = ({ editHandler }) => {
+  return (
+    <>
+      <button className="edit-diary-btn" onClick={editHandler} type="button">
+        Edit
+      </button>
+    </>
+  );
+};
+
+const DeleteDiaryButton = ({ btnClassName, deleteHandler }) => {
+  return (
+    <>
+      <button className={btnClassName} onClick={deleteHandler} type="button">
+        X
+      </button>
+    </>
   );
 };
 
 // Diary card component
-const Diary = ({ diary }) => {
+const Diary = ({ diary, canEdit, deleteBtnHandler }) => {
   const diaryId = diary.id;
+  const diaryName = diary.diary_name;
+
+  const btnClassName = canEdit ? "delete-diary-btn" : "delete-diary-btn-hidden";
+  const deleteDiary = () => {
+    deleteBtnHandler(diaryName);
+  };
 
   return (
     <div className="diary-card-container">
+      <DeleteDiaryButton btnClassName={btnClassName} deleteHandler={deleteDiary} />
       <div className="diary-image-container">
         <img src={diaryImg} alt="diary"></img>
       </div>
       <div className="diary-title">
-        <h3>{diary.diary_name}</h3>
+        <h3>{diaryName}</h3>
       </div>
       <div className="diary-description">
         <p>This is the decription of the diary</p>
@@ -39,12 +66,12 @@ const Diary = ({ diary }) => {
 };
 
 // Diary card grouping component
-const DiaryList = ({ diaries }) => {
+const DiaryList = ({ diaries, canEdit, deleteHandler }) => {
   return (
     <div className="diary-card-list">
       {diaries.map((diary) => {
         console.log("reloading List");
-        return <Diary diary={diary} />;
+        return <Diary diary={diary} canEdit={canEdit} deleteBtnHandler={deleteHandler} />;
       })}
     </div>
   );
@@ -54,11 +81,23 @@ const DiaryList = ({ diaries }) => {
 const DiaryPage = () => {
   const [diaries, setDiaries] = useState([]);
   const [isVisibleModal, setVisibleModal] = useState(false);
+  const [isEditable, setEditable] = useState(false);
 
   // event handler for "Create a Diary" button
   const createButtonHandler = (event) => {
     setVisibleModal(true);
     console.log("popup");
+  };
+
+  const editButtonHandler = (event) => {
+    setEditable(!isEditable);
+    console.log("show delete buttons");
+  };
+
+  const deleteButtonHandler = (diaryName) => {
+    DiaryService.deleteDiary(diaryName)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
 
   // fetch all diaries from database - no page reload
@@ -74,12 +113,14 @@ const DiaryPage = () => {
   return (
     <div className="diary-hub">
       <h1 className="diary-hub-heading">Diaries</h1>
-      {isVisibleModal ? (
-        <DiaryModal />
-      ) : (
+      {() => {
+        if (isVisibleModal) return <DiaryModal />;
+      }}
+      <div className="modal-popup-btn-container">
         <CreateDiaryButton createBtnHandler={createButtonHandler} />
-      )}
-      <DiaryList diaries={diaries} />
+        <EditDiaryButton editHandler={editButtonHandler} />
+      </div>
+      <DiaryList diaries={diaries} canEdit={isEditable} deleteHandler={deleteButtonHandler} />
     </div>
   );
 };
