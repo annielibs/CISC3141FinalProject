ff4b7014db729dfbab12b4f0799e204b73beb13b
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2021-12-19 18:34:47 -0500

    server only

--- diary/src/api/index.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-
-const instance = axios.create({
-  baseURL: `https://jurnl-application.herokuapp.com/api`,
-  headers: { "Content-type": "application/json" },
-});
-
-module.exports = instance;
